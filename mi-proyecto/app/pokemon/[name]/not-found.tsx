b9c82d1f3afeec10a0c4ae303c66e8e31270927c
Franco <frannvallone@gmail.com>
2025-10-20 00:44:45 -0300

    Ahora uso el BackButton

--- mi-proyecto/app/pokemon/[name]/not-found.tsx
+++ mi-proyecto/app/pokemon/[name]/not-found.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
 import Image from "next/image";
+import BackButton from "@/components/backButton";
 
 export default function NotFound() {
     return (
@@ -10,12 +10,7 @@ export default function NotFound() {
                     <p className="text-gray-600 mb-8">
                         El Pokémon que buscas no existe o no está disponible.
                     </p>
-                    <Link
-                        href="/"
-                        className="inline-block bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-600 font-semibold"
-                    >
-                        Volver a la Pokédex
-                    </Link>
+                    <BackButton/>
                 </div>
                 <div>
                     <Image height={400} width={400} src={"/psyduck.png"} alt={"Psyduck"}/>
